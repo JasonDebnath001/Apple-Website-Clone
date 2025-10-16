5c8aa283b4b2f4e888b6f8c20155749edec0fc8f
JasonDebnath001 <aritradebnath1@gmail.com>
2025-10-16 19:32:33 +0530

    Add model variant/color selection logic
    The 3D model can now be dynamically updated. This sets the stage for Ui elements to control the product's appearance in real-time.

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -5,9 +5,12 @@ import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14";
 import StudioLights from "./StudioLights";
+import ModelSwitcher from "./three/ModelSwitcher.jsx";
+import { useMediaQuery } from "react-responsive";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacBookStore();
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
 
   return (
     <section id="product-viewer">
@@ -15,7 +18,7 @@ const ProductViewer = () => {
 
       <div className="controls">
         <p className="info">
-          MacBook Pro {scale} in {color}
+          MacBook Pro available in 14" & 16" in Space Gray & Dark colors
         </p>
 
         <div className="flex-center gap-5 mt-5">
@@ -67,9 +70,10 @@ const ProductViewer = () => {
       >
         <StudioLights />
 
-        <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-
-        <OrbitControls enableZoom={false} />
+        <ModelSwitcher
+          scale={isMobile ? scale - 0.03 : scale}
+          isMobile={isMobile}
+        />
       </Canvas>
     </section>
   );
